f870666d1054c15e53d711d83cacedd52dadc107
ronniekram <arynn.boniface@gmail.com>
2020-08-08 18:18:03 -0700

    updated actions

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import RootReducer from './reducers/RootReducer'
+import RootReducer from './reducers/index'
 
 const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
   document.getElementById('root'));
