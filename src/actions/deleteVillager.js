adc4ad94c70d18da4149ebbff69016852fa27cbe
ronniekram <arynn.boniface@gmail.com>
2020-08-09 19:02:42 -0700

    finished writing actions

--- src/actions/deleteVillager.js
+++ src/actions/deleteVillager.js
@@ -1,3 +1,12 @@
 export const deleteVillager = (villagerId, islandId) => {
-  
+  return (dispatch) => {
+    return fetch(`http://localhost:3000/islands/${islandId}/villagers/${villagerId}`, {
+      method: 'DELETE'
+    })
+    .then(resp => resp.json())
+    .then(island => dispatch({
+      type: 'DELETE_VILLAGER',
+      payload: island
+    }))
+  }
 }
