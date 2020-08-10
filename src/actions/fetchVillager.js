258d50a5e4f7f3b80449406d67cbf28f27dbcb46
ronniekram <arynn.boniface@gmail.com>
2020-08-09 19:20:07 -0700

    implemented search function

--- src/actions/fetchVillager.js
+++ src/actions/fetchVillager.js
@@ -1,6 +1,6 @@
-export const fetchVillager = (searchTerm) => {
+export const fetchVillager = (query) => {
   return (dispatch) => {
-    fetch(`https://nookipedia.com/api/villager/${searchTerm}/?api_key=a0f9c0e9-e1a3-4bb8-a216-25791670ffc4`)
+    fetch(`https://nookipedia.com/api/villager/${query}/?api_key=a0f9c0e9-e1a3-4bb8-a216-25791670ffc4`)
     .then(resp => resp.json())
     .then(villager => dispatch({
       type: 'FETCH_VILLAGER',
@@ -8,3 +8,5 @@ export const fetchVillager = (searchTerm) => {
     }))
   }
 }
+
+// add alert when villager does not exist
--- src/components/villagers/VillagerSearch.js
+++ src/components/villagers/VillagerSearch.js
@@ -1,13 +1,36 @@
 import React from 'react';
-// import { Route } from 'react-router-dom'
+import {connect} from 'react-redux';
+import {fetchVillager} from '/Users/HotRonnie/Desktop/ac-villagers-react/villager-front/src/actions/fetchVillager.js'
 
-const VillagerSearch = () => {
-  return (
-    <form>
-      <input type="search" />
-      <button>Find Villager</button>
-    </form>
-  )
+class VillagerSearch extends React.Component {
+  state = {
+    query: ""
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.fetchVillager(this.state);
+    this.setState({
+      query:''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="query" value={this.state.query} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default VillagerSearch;
+export default connect(null, {fetchVillager})(VillagerSearch);
