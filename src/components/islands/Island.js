533ddb560e3d1cdd93e7be723457432343d206fe
ronniekram <arynn.boniface@gmail.com>
2020-08-10 16:10:08 -0700

    up to date for my favorite pal to peek at

--- src/components/islands/Island.js
+++ src/components/islands/Island.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+// import {Redirect} from 'react-router-dom';
 import VillagersContainer from '/Users/HotRonnie/Desktop/ac-villagers-react/villager-front/src/containers/VillagersContainer.js'
 
 
