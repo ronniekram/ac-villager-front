ca6d76a37cfbb29a835f01ad807c6d453c618512
ronniekram <arynn.boniface@gmail.com>
2020-08-11 14:18:14 -0700

    search bar not working yet

--- src/components/villagers/VillagerSearch.js
+++ src/components/villagers/VillagerSearch.js
@@ -1,50 +1,25 @@
 import React, { useEffect, useState } from 'react';
-// import {connect} from 'react-redux';
-// import {fetchVillagers} from '/Users/HotRonnie/Desktop/ac-villagers-react/villager-front/src/actions/fetchVillagers.js'
+// import Villagers from './Villagers';
 
-// class VillagerSearch extends React.Component {
-//   state = {
-//     query: '',
-//   }
-
-//   handleChange = event => {
-//     this.setState({
-//       query: event.target.value
-//     })
-//   }
-//     render() {
-//     return (
-//       <div>
-//         <input type="text" className="searchbox" placeholder="Search villagers by name..." onChange={(event) => this.handleChange(event)}/>
-//     <p>{this.state.query}</p>
-//       </div>
-//     )
-//     }
-//   }
  function VillagerSearch({villagers}) {
-   const [searchTerm, setSearch] = React.useState("")
-   const [searchResults, setResults] = React.useState([])
-   const handleChange = event => {
-     setSearch(event.target.value);
-    };
+    // const [searchTerm, setSearch] = useState("")
+    // const [searchResults, setResults] = useState({})
+    // const handleChange = event => {
+    //  setSearch(event.target.value);
+    // };
 
-    React.useEffect(() => {
-      const results = villagers.filter(villager => 
-          villager.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setResults(results);
-    }, [searchTerm]);
+
+    // useEffect(() => {
+    //   const results = villagers.filter(villager =>
+    //     villager.name.toLowerCase().includes(searchTerm.toLowerCase())
+    //   );
+    //   setResults(results);
+    // }, [villagers, searchTerm]);
 
 
     return (
       <div className="searchbar">
         <input type="text" placeholder="Search villager by name..." value={searchTerm} onChange={handleChange} />
-
-        <ul>
-          {searchResults.map(villager => (
-            <li>{villager.name}</li>
-          ))}
-        </ul>
       </div>
     )
  }
