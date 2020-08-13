bf1ec3676598ebab1e253680bd6698bf1b3f0ab3
ronniekram <arynn.boniface@gmail.com>
2020-08-13 00:21:23 -0700

    hooks working on all fronts. i'm hooked baby

--- src/components/wishlists/Wishlists.js
+++ src/components/wishlists/Wishlists.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-// const Wishlists = (props) => {
-//   const list = props.wishlists.map(wishlist => 
-//     <li key={wishlist.id}> 
-//       <Link to={`/wishlists/${wishlist.id}`}> {wishlist.name}</Link>
-//     </li>
-//   )
-
-//   return (
-//     <>
-//       <h3>Wishlists</h3>
-//         <ul>
-//           {list}
-//         </ul>
-//     </>
-//   )
-// }
-
-// export default Wishlists;
-
-
 function Wishlists({wishlists}) {
+  console.log(wishlists)
   const list = wishlists && wishlists.map(wishlist => 
     <li key={wishlist.id}> 
       <Link to={`/wishlists/${wishlist.id}`}> {wishlist.name}</Link>
-    </li>
-  )
+    </li> )
 
   return (
     <>
--- src/containers/wishlistContainer.js
+++ src/containers/wishlistContainer.js
@@ -1,54 +1,3 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import {Route, Switch} from 'react-router-dom';
-// import {fetchWishlists, addWishlist, deleteWishlist} from '../actions/wishlistActions';
-// import {fetchVillagers} from '../actions/villagerActions'
-// import Wishlist from '../components/wishlists/Wishlist'
-// import Wishlists from '../components/wishlists/Wishlists'
-// import wishlistInput from '../components/wishlists/wishlistInput';
-
-
-// class WishlistContainer extends React.Component {
-
-//   componentDidMount() {
-//     this.props.fetchWishlists()
-//     this.props.fetchVillagers()
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <Switch>
-//           <Route path='/wishlists/new' 
-//             component={wishlistInput} addWishlist={addWishlist} />
-
-//           <Route exact path='/wishlists/:id'
-//             render={(routerProps) => 
-//               <Wishlist {...routerProps} 
-//                 wishlists={this.props.wishlists} />}/>
-
-
-//           <Route path='/wishlists' 
-//             render={(routerProps) => 
-//               <Wishlists {...routerProps} 
-//                 wishlists={this.props.wishlists} />} />
-//         </Switch>
-//       </>
-//     )
-//   }
-
-
-// }
-
-// const mapStateToProps = state => {
-//   return {
-//     wishlists: state.wishlistReducer.wishlists
-//   }
-// }
-
-// export default connect(mapStateToProps, {fetchWishlists, addWishlist, deleteWishlist, fetchVillagers})(WishlistContainer);
-
-
 import React from 'react';
 import { connect } from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
@@ -56,7 +5,6 @@ import {fetchWishlists} from '../actions/wishlistActions';
 import Wishlist from '../components/wishlists/Wishlist'
 import Wishlists from '../components/wishlists/Wishlists'
 import WishlistInput from '../components/wishlists/wishlistInput';
-
 
 function WishlistContainer({history, match, wishlists}) {
 
@@ -83,7 +31,7 @@ function WishlistContainer({history, match, wishlists}) {
 
 const mapStateToProps = state => {
   return {
-    islands: state.wishlistReducer.wishlists
+    wishlists: state.wishlistReducer.wishlists
   }
 }
 
