02bab34e932c8b53bf8ca40d760cf628c5bb0f32
ronniekram <arynn.boniface@gmail.com>
2020-08-14 14:23:25 -0700

    page refreshes after villager remove

--- src/components/villagers/Villagers.js
+++ src/components/villagers/Villagers.js
@@ -7,18 +7,18 @@ import Villager from './Villager'
 function Villagers({removeVillager, villagers, history, match}) {
 
   const handleRemove = (villager, island) => {
-    history.push(`/islands/${island}`)
     removeVillager(villager, island)
+    window.location.reload(true);
   }
   
   const list = villagers && villagers.map(villager =>
     <li key={villager.id}> <Link to={`/villagers/${villager.id}`} villagers={villagers}> {villager.name} </Link> <button onClick={() => handleRemove(villager.id, villager.island_id)} > x </button> </li>)
   return (
     <>
-    <Route path="/villagers/:id" render={(routerProps) => <Villager {...routerProps} villagers={villagers} />} />
-    <ul>
-      {list}
-    </ul>
+      <Route path="/villagers/:id" render={(routerProps) => <Villager {...routerProps} villagers={villagers} />} />
+      <ul>
+        {list}
+      </ul>
     </>
   )
 }
