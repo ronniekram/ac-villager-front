6e1d21365d60f34bc7c3d6d12b0880b973deee0d
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-01 15:06:52 -0500

    attempting to fix image error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import AC2 from './img/AC2.jpg';
+// import AC2 from './img/AC2.jpg';
 // import { Row } from 'react-bootstrap';
 
 function Header() {
+  let imageLink = "https://raw.githubusercontent.com/ronniekram/ac-villager-front/master/src/components/img/AC2.jpg"
   return (
     <>
     <div className="header-container">
-        <img src={AC2} alt="animal crossing" className="nav-img" />
+        <img src={imageLink} alt="animal crossing" className="nav-img" />
     </div>
     </>
   )
