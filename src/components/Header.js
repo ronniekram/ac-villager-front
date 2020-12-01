509ba77622c688bbe9eb1aa1e14a14dc26dfc692
Arynn Boniface <arynn.boniface@gmail.com>
2020-12-01 15:14:31 -0500

    still having issues with header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import AC2 from './img/AC2.jpg';
+import AC2 from './img/AC2.jpg';
 // import { Row } from 'react-bootstrap';
 
 function Header() {
-  let imageLink = "https://raw.githubusercontent.com/ronniekram/ac-villager-front/master/src/components/img/AC2.jpg"
+  // let imageLink = "https://i.imgur.com/RcLWdWZ.jpeg"
   return (
     <>
     <div className="header-container">
-        <img src={imageLink} alt="animal crossing" className="nav-img" />
+        <img src={AC2} alt="animal crossing" className="nav-img" />
     </div>
     </>
   )
